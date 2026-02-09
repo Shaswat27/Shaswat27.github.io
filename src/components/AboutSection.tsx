1b12a3c0a4c3146e1a82830e423956211f6c1f31
Shaswat27 <shaswat.datta@gmail.com>
2026-02-09 11:29:58 -0500

    Fix: scroll speed of tech stack on mobile

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
           <div className="relative">
             <div className="absolute left-0 top-0 bottom-0 w-20 bg-gradient-to-r from-background to-transparent z-10" />
             <div className="absolute right-0 top-0 bottom-0 w-20 bg-gradient-to-l from-background to-transparent z-10" />
-            <div className="flex gap-4 marquee">
+            <div className="flex gap-4 animate-marquee-fast md:animate-marquee-slow">
               {[...techStack, ...techStack].map((tech, i) => (
                 <span
                   key={i}
